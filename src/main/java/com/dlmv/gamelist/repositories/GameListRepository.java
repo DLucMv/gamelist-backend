46eeb81a21332ff796a7ba3f9e45fb7450d9f36b
DLucMv <dlucasmv@gmail.com>
2023-05-14 21:04:53 -0300

    Move games on list

--- src/main/java/com/dlmv/gamelist/repositories/GameListRepository.java
+++ src/main/java/com/dlmv/gamelist/repositories/GameListRepository.java
@@ -3,8 +3,16 @@ package com.dlmv.gamelist.repositories;
 
 import com.dlmv.gamelist.entities.GameList;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 //Nosso Repository é o componente responsavél por fazer consultas ao db.
 public interface GameListRepository extends JpaRepository<GameList, Long> {
+
+    @Modifying
+    @Query(nativeQuery = true,
+            value = "UPDATE tb_belonging SET position = :newPosition WHERE list_id = :listId AND game_id = :gameId")
+    void updateBelongingPosition(Long listId, Long gameId, Integer newPosition);
+
 }
 
--- src/main/java/com/dlmv/gamelist/services/GameListService.java
+++ src/main/java/com/dlmv/gamelist/services/GameListService.java
@@ -3,7 +3,9 @@ package com.dlmv.gamelist.services;
 
 import com.dlmv.gamelist.dto.GameListDTO;
 import com.dlmv.gamelist.entities.GameList;
+import com.dlmv.gamelist.projections.GameMinProjection;
 import com.dlmv.gamelist.repositories.GameListRepository;
+import com.dlmv.gamelist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,12 +19,33 @@ public class GameListService {
     @Autowired //Injetando dependencia
     private GameListRepository gameListRepository;
 
+    @Autowired
+    private GameRepository gameRepository;
+
     @Transactional(readOnly = true)
     public List<GameListDTO> findAll(){
         List<GameList> result = gameListRepository.findAll();
         return result.stream().
                 map(x -> new GameListDTO(x)).toList();
                 //Obj Game -> Obj GameMinDTO
+    }
+
+    @Transactional(readOnly = true)
+    public void move(Long listId, int sourceIndex, int destinationIndex){
+
+        List<GameMinProjection> list = gameRepository.searchByList(listId);
+
+        GameMinProjection obj = list.remove(sourceIndex);
+        list.add(destinationIndex, obj);
+
+        int min = Math.min(sourceIndex, destinationIndex);
+        int max = Math.max(sourceIndex, destinationIndex);
+
+        for (int i = 0; i < max; i++) {
+            gameListRepository.
+                    updateBelongingPosition(listId, list.get(i).getId(), i);
+        }
+
     }
 
 }
